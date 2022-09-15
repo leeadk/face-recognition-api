0c6f46f4d144bdcf0e23ad82bafef9313c2ce18e
Leead <leeadk@gmail.com>
2022-09-15 10:18:32 +0300

    Updating login and register in backend

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const bcrypt = require('bcryptjs');
 const knex = require('knex');
 
 const WSL_PROCESS_ENV = process.env.WINDOWS_HOST;
-const PSQL_PORT = '5432';
+const PSQL_USER_DB = 'users';
+const PSQL_LOGIN_DB = 'login';
 const PSQL_USR = 'postgres';
 const PSQL_DB = 'postgres';
+const PSQL_PORT = 5432;
 const APP_PORT = 5000;
 const BCRYPT_SALT_ROUNDS = 10;
+
 
 const app = express();
 app.listen(APP_PORT);
@@ -39,6 +42,20 @@ app.get('/signin', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
+    const { email, password } = req.body;
+    postgres(PSQL_LOGIN_DB).where(({ email: email })).first().then(user => {
+        bcrypt.compare(password, user.hash, function (err, result) {
+            if (err) {
+                res.status(400).json('Bcrypt err');
+            }
+            if (result === true) {
+                res.status(200).json('Success');
+            }
+            else {
+                res.status(400).json('Failed to authenticate');
+            }
+        });
+    });
 });
 
 app.get('/register', (req, res) => {
@@ -52,24 +69,22 @@ app.post('/register', (req, res) => {
         if (err) {
             res.status(400).json('Bcrypt failed, no changes saved');
         }
-        postgres('users')
-            .returning('*')
-            .insert({
-                name: name,
-                email: email,
-                passwd: hash,
-                created_on: new Date()
-            }).then(user => {
-                res.json(user[0]);
-            }).catch(err => {
-                res.status(400).json('Unable to register');
-            });
+        postgres(PSQL_LOGIN_DB).insert({
+            hash: hash,
+            email: email
+        }).then(() => {
+            postgres(PSQL_USER_DB).returning('*')
+                .insert({
+                    name: name,
+                    email: email,
+                    created_on: new Date()
+                }).then(user => {
+                    res.json(user[0]);
+                })
+        }).catch(err => {
+            res.status(400).json('Unable to register');
+        })
     })
-});
-
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-
 });
 
 app.put('/image', (req, res) => {
